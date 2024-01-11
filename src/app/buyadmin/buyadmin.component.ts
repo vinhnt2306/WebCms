d9a9e44101e83b559964430431db2badfb2adefb
vinhnt2306 <ntvinh2306@gmail.com>
2024-01-11 16:52:24 +0700

    update;update

--- src/app/buyadmin/buyadmin.component.ts
+++ src/app/buyadmin/buyadmin.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from 'src/core/product';
 import { ProductService } from '../service/product.service';
 import { CartegoryService } from '../service/cartegory.service';
@@ -8,57 +8,79 @@ import { CartegoryService } from '../service/cartegory.service';
   styleUrls: ['./buyadmin.component.css']
 })
 export class BuyadminComponent {
-  
-@Input()  product!: Product;
-  products : Product[] = [];
-  pdSearch : Product[]=[];
-  searchText: string = '';
-  cartProducts : any;
-  productDetail : Product = new Product;
 
-  constructor(private productService:ProductService,
-              private cartService:CartegoryService){}
+  @Input() product!: Product;
+  products: Product[] = [];
+  pdSearch: Product[] = [];
+  sum = 0;
+  numberOfProduct = 0;
+  searchText: string = '';
+  cartProducts: any;
+  productDetail: Product = new Product;
+  cartProductsByPayment: any[] = []; //sản phẩm trong giỏ hàng CartItem
+
+  constructor(private productService: ProductService,
+    private cartService: CartegoryService) { }
   ngOnInit(): void {
+    var lstCart: any = [];
+
     this.loadProducts();
-  //   this.productService.getCartItem().subscribe((response: any) => {
-  //     this.cartProducts = response.data.cartItem
-  //     console.log(this.cartProducts)
-  // })
+    //   this.productService.getCartItem().subscribe((response: any) => {
+    //     this.cartProducts = response.data.cartItem
+    //     console.log(this.cartProducts)
+    // })
+
+    this.productService.getCartItem().subscribe((response: any) => {
+      this.cartProductsByPayment = response.data.cartItem;
+      lstCart = response.data.cartItem;
+      //Tính số lượng * đơn giá
+      this.numberOfProduct = response.data.cartItem.length;
+      this.sum = response.data.cartItem.reduce((next: any, prev: any) => {
+        return next + prev.price * prev.quantity;
+      }, 0);
+    });
   }
   loadProducts(): void {
     this.productService.getListProduct().subscribe((response: any) => {
       this.products = response.data.lstProduct
       console.log(this.products)
-  });
+    });
   }
   loadProductsSearch(): void {
     this.productService.getListProductSearch(this.searchText).subscribe((response: any) => {
       this.products = response.data.lstProduct
       console.log(this.products)
-  });
+    });
   }
   addToCartProduct(productId: string, quantity: number) {
-  this.productService.addToCart(productId, quantity, true)
-    .subscribe(
-      data => {
-        console.log(data);
-        if(data.status == "200"){
+    this.productService.addToCart(productId, quantity, true)
+      .subscribe(
+        data => {
+          console.log(data);
+          if (data.status == "200") {
             alert('thêm sản phẩm vào giỏ hàng thành công');
-        }else{
-          alert('Sản phẩm không đủ số lượng để thêm vui lòng liên hệ cửa hàng.');
+            this.productService.getCartItem().subscribe((response: any) => {
+              this.cartProductsByPayment = response.data.cartItem;
+              this.numberOfProduct = response.data.cartItem.length;
+              this.sum = response.data.cartItem.reduce((next: any, prev: any) => {
+                return next + prev.price * prev.quantity;
+              }, 0);
+            });
+          } else {
+            alert('Sản phẩm không đủ số lượng để thêm vui lòng liên hệ cửa hàng.');
+          }
+        },
+        error => {
+          console.error('Lỗi khi thêm sản phẩm vào giỏ hàng:', error);
+          // Xử lý lỗi ở đây, ví dụ: hiển thị thông báo lỗi cho người dùng
+          alert('Sản phẩm đã hết hàng.');
         }
-      },
-      error => {
-        console.error('Lỗi khi thêm sản phẩm vào giỏ hàng:', error);
-        // Xử lý lỗi ở đây, ví dụ: hiển thị thông báo lỗi cho người dùng
-        alert('Sản phẩm đã hết hàng.');
-      }
-    );
-  console.log(productId, quantity);
-}
-searchProducts() {
-  // Gọi service để tìm kiếm sản phẩm dựa trên searchText
-  // Lưu ý: Cần phải triển khai service để thực hiện chức năng tìm kiếm
-  // Ví dụ: this.productService.searchProducts(this.searchText).subscribe(result => { this.productList = result; });
-}
+      );
+    console.log(productId, quantity);
+  }
+  searchProducts() {
+    // Gọi service để tìm kiếm sản phẩm dựa trên searchText
+    // Lưu ý: Cần phải triển khai service để thực hiện chức năng tìm kiếm
+    // Ví dụ: this.productService.searchProducts(this.searchText).subscribe(result => { this.productList = result; });
+  }
 }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
-import { Product } from 'src/core/product';
+import { Product, ProductUpdate } from 'src/core/product';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +9,12 @@ import { Product } from 'src/core/product';
 export class ProductService {
   private baseURL = 'https://localhost:44383';
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   //get product
   getListProduct(): Observable<any> {
     const data = {
-      "":""
+      "": ""
 
     }
     return this.httpClient.request('POST', `${this.baseURL}/api/HomePage/Process`, {
@@ -34,35 +34,53 @@ export class ProductService {
     })
   }
   //tạo mới sản phẩm
-  createProduct(product : Product) : Observable<any>{
-    return this.httpClient.post(`${this.baseURL}/api/CreateProduct/Process`,{...product,
-      token : JSON.parse(localStorage.getItem('currentUser')??"").data.token,
-      TypeImage : "1"
+  createProduct(product: Product): Observable<any> {
+    return this.httpClient.post(`${this.baseURL}/api/CreateProduct/Process`, {
+      ...product,
+      token: JSON.parse(localStorage.getItem('currentUser') ?? "").data.token,
+      TypeImage: "1"
+    });
+  }
+  updateProduct(product: ProductUpdate): Observable<any> {
+    return this.httpClient.post(`${this.baseURL}/api/EditProduct/Process`, {
+      ...product,
+      // iD: id,
+      token: JSON.parse(localStorage.getItem('currentUser') ?? "").data.token,
+      TypeImage: "1"
+    });
+  }
+  getProductDetail(id: string): Observable<any> {
+    return this.httpClient.post(`${this.baseURL}/api/DetailProduct/Process`, {
+      iD: id,
+      token: JSON.parse(localStorage.getItem('currentUser') ?? "").data.token,
+      TypeImage: "1"
     });
   }
   //
-  addToCart(productId: string, quantity: number,LoginType:boolean): Observable<any> {
+  addToCart(productId: string, quantity: number, LoginType: boolean): Observable<any> {
     const body = {
       ProductId: productId,
       Quantity: quantity,
-      LoginType:LoginType=false,
-      token : JSON.parse(localStorage.getItem('currentUser')??"").data.token
+      Type: null,
+      LoginType: true,
+      token: JSON.parse(localStorage.getItem('currentUser') ?? "").data.token
     };
     return this.httpClient.post(`${this.baseURL}/api/AddToCart/Process`, body)
     // return this.httpClient.post(`${this.baseURL}/api/AddToCart/Process`, body);
   }
 
-  getCartItem() : Observable<any>{
+  getCartItem(): Observable<any> {
     let data = {
-      token : JSON.parse(localStorage.getItem('currentUser')??"").data.token
+      token: JSON.parse(localStorage.getItem('currentUser') ?? "").data.token,
+      LoginType: true,
     }
-    return this.httpClient.request('POST',`${this.baseURL}/api/CartItem/Process`,
-    {
-      body : data,
-      observe:'body',
-      responseType : 'json',
+    return this.httpClient.request('POST', `${this.baseURL}/api/CartItem/Process`,
+      {
+        body: data,
+        observe: 'body',
+        responseType: 'json',
 
-    })
+      })
   }
 
   upLoadImage(data: FormData) {
